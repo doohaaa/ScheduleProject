ec0eddda01c22ce58b39f75222fcacc1591b4d2e
doohaaa <engk5423@naver.com>
2025-05-20 16:54:24 +0900

    Lv1. 일정 CRUD - 일정 Entity 설계

---
+++ src/main/java/com/example/scheduleproject/entity/Schedule.java
@@ -0,0 +1,22 @@
+package com.example.scheduleproject.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="schedule")
+public class Schedule extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String contents;
+
+}
